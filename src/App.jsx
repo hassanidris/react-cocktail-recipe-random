90c3676e832df8de2ebd043b4705b90a13f134cd
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-18 23:39:35 +0100

    fixing button

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       <div className="max-w-4xl mx-auto px-4 py-10">
         <div className=" flex flex-col justify-center items-center gap-3">
           <p>Click the button to check a random cocktail recipe for your after work</p>
-          <button onClick={() => fetchData()} className=" bg-gray-800 text-white px-4 py-2 w-full rounded-md md:w-60">Get for New Cocktail</button>
+          <button onClick={() => fetchData()} className=" bg-gray-800 text-white px-4 py-2 w-full rounded-md md:w-60">Get New Cocktail</button>
         </div>
         <div className=" bg-orange-50 p-12 mt-8 rounded-md">
           <h1 className="text-4xl font-bold underline">{strDrink}</h1>
